f55574f94c6c60c18dc6cb37160c761c8555b455
nyoandnuraco <info@nyoandnuraco.com>
2020-04-06 22:49:21 -0500

    add:bootstrap

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,14 +3,13 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-blog-post-js": () => import("./../src/templates/blog-post.js" /* webpackChunkName: "component---src-templates-blog-post-js" */),
-  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-blog-js": () => import("./../src/pages/blog.js" /* webpackChunkName: "component---src-pages-blog-js" */),
   "component---src-pages-cart-js": () => import("./../src/pages/cart.js" /* webpackChunkName: "component---src-pages-cart-js" */),
   "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-ourwork-js": () => import("./../src/pages/ourwork.js" /* webpackChunkName: "component---src-pages-ourwork-js" */),
   "component---src-pages-shop-js": () => import("./../src/pages/shop.js" /* webpackChunkName: "component---src-pages-shop-js" */),
-  "component---src-pages-shop-neldolight-js": () => import("./../src/pages/shop/neldolight.js" /* webpackChunkName: "component---src-pages-shop-neldolight-js" */),
   "component---src-pages-shop-neldo-js": () => import("./../src/pages/shop/neldo.js" /* webpackChunkName: "component---src-pages-shop-neldo-js" */),
-  "component---src-pages-shop-neldochamp-js": () => import("./../src/pages/shop/neldochamp.js" /* webpackChunkName: "component---src-pages-shop-neldochamp-js" */)
+  "component---src-pages-shop-neldochamp-js": () => import("./../src/pages/shop/neldochamp.js" /* webpackChunkName: "component---src-pages-shop-neldochamp-js" */),
+  "component---src-pages-shop-neldolight-js": () => import("./../src/pages/shop/neldolight.js" /* webpackChunkName: "component---src-pages-shop-neldolight-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,14 +6,13 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/templates/blog-post.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/.cache/dev-404-page.js"))),
   "component---src-pages-blog-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/blog.js"))),
   "component---src-pages-cart-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/cart.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/index.js"))),
   "component---src-pages-ourwork-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/ourwork.js"))),
   "component---src-pages-shop-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/shop.js"))),
-  "component---src-pages-shop-neldolight-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/shop/neldolight.js"))),
   "component---src-pages-shop-neldo-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/shop/neldo.js"))),
-  "component---src-pages-shop-neldochamp-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/shop/neldochamp.js")))
+  "component---src-pages-shop-neldochamp-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/shop/neldochamp.js"))),
+  "component---src-pages-shop-neldolight-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/shop/neldolight.js")))
 }
 
