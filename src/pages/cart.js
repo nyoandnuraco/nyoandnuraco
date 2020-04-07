5108f73d3d5ca3ce3125041c945254523c0c9f9c
nyoandnuraco <info@nyoandnuraco.com>
2020-04-07 03:30:05 -0500

    add:checkoutstepone

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-//import './shop.css'
+import styles from './cart.module.css'
 import Layout from '../components/layout'
 
 
@@ -8,7 +8,18 @@ class Cart extends React.Component {
   
     return (
       <Layout location={this.props.location}>
-       
+        <br/>
+       <h1 className={styles.header}>Checkout</h1>
+       <br/>
+       <div className={styles.checkout}>
+        <span id={styles.active} className={styles.step}>1</span>
+        <div className={styles.progressbar}></div>
+        <span className={styles.step}>2</span>
+        <div className={styles.progressbar}></div>
+        <span className={styles.step}>3</span>
+        <div className={styles.progressbar}></div>
+        <span className={styles.step}>4</span>
+       </div>
       </Layout>
     )
   }
