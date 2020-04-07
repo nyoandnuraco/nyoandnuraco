afeb3d54573ec6faa13a10cbcaec0d6742f06e9b
nyoandnuraco <info@nyoandnuraco.com>
2020-04-07 00:22:45 -0500

    fix:navbarwidthandplmargin

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -10,32 +10,33 @@ class Shop extends React.Component {
     return (
       <Layout location={this.props.location}>
             <br/>
-          <div className={styles.hero}><h1 className={styles.landingheader}>Shop</h1></div>
-        <div className="container pl-container">
       
-       <div>
+          <div className={styles.hero}><h1 className={styles.landingheader}>Shop</h1></div>
+     
+          <div className={styles.plcontainer}>
+          <hr/>
        <Link to="/shop/neldolight">
          <h3>Neldo Light</h3>
          <p>dsfasdfafdasfsadfasfdas</p>
          <span>$2500</span>
        </Link>
-       </div>
+   
        <hr/>
-       <div className="">
+      
        <Link to="/shop/neldo">
           <h3>Neldo</h3>
           <p>dsfasdfafdasfsadfasfdas</p>
           <span>$2500</span>
       </Link>
-       </div>
+   
        <hr/>
-       <div className="">
+  
        <Link to="/shop/neldochamp">
           <h3>Neldo Champ</h3>
           <p>dsfasdfafdasfsadfasfdas</p>
           <span>$2500</span>
        </Link>
-       </div>
+    
        <hr/>
        </div>
       </Layout>
