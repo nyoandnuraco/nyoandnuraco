8b89bbe13d2e0ae4670530d681f26902867e0925
nyoandnuraco <info@nyoandnuraco.com>
2020-04-07 04:19:16 -0500

    add:stylesstep1

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,29 +1,69 @@
-import React from 'react'
-import styles from './cart.module.css'
-import Layout from '../components/layout'
-
+import React from "react";
+import styles from "./cart.module.css";
+import Layout from "../components/layout";
+import logo from "../components/nyoandnuralogo.png";
 
 class Cart extends React.Component {
   render() {
-  
     return (
       <Layout location={this.props.location}>
-        <br/>
-       <h1 className={styles.header}>Checkout</h1>
-       <br/>
-       <div className={styles.checkout}>
-        <span id={styles.active} className={styles.step}>1</span>
-        <div className={styles.progressbar}></div>
-        <span className={styles.step}>2</span>
-        <div className={styles.progressbar}></div>
-        <span className={styles.step}>3</span>
-        <div className={styles.progressbar}></div>
-        <span className={styles.step}>4</span>
-       </div>
+        <br />
+        <h1 className={styles.header}>Checkout</h1>
+        <br />
+        <div className="container">
+          <div className={styles.checkout}>
+            <span id={styles.active} className={styles.step}>
+              1
+            </span>
+            <div className={styles.progressbar}></div>
+            <span className={styles.step}>2</span>
+            <div className={styles.progressbar}></div>
+            <span className={styles.step}>3</span>
+            <div className={styles.progressbar}></div>
+            <span className={styles.step}>4</span>
+          </div>
+          <br /><br/>
+          <div className={styles.sidebyside}>
+          <h2>Your Cart</h2>
+          <h4>1 Item</h4>
+          </div>
+          <hr />
+          <h6 className={styles.cartitemdesc}>
+            Neldo Package: Lorem ipsum ipsum ipsum
+          </h6>
+          <img className={styles.cartitemimg} src={logo} />
+          <p className={styles.cartitemqty}>
+            <span><strong>Quantity: </strong></span>
+            <select className={styles.cartitemselect}>
+              <option> 1</option>
+            </select>
+          </p>
+          <p className={styles.pricelabel}>
+            <span>
+              <strong>Price: </strong>
+            </span>
+            <span>
+              <i>$2999.00</i>
+            </span>
+          </p>
+          <br /> <br /> <br />
+          <button className={styles.removecta}>Remove Item</button>
+          <br /> <br /> 
+          <hr />
+    
+          <p>
+            Total:
+            <span>
+              <i>$2999.00</i>
+            </span>
+          </p>
+          <button className={styles.secondarycta}>Continue to Checkout</button>
+          <br />
+        </div>
+        <br /> <br />
       </Layout>
-    )
+    );
   }
 }
 
-export default Cart
-
+export default Cart;
