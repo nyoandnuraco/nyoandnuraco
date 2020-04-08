3b90ba53829a8d9f909abd8c75a327b5c80beb29
nyoandnuraco <info@nyoandnuraco.com>
2020-04-07 20:06:33 -0500

    datepicker

--- src/pages/book.js
+++ src/pages/book.js
@@ -25,65 +25,10 @@ class Book extends React.Component {
           </div>
           <br /><br/>
           <h2>Book Initial Consultation</h2>
-          <br/><br/><br/><br/>  
+          <br/><br/> 
           <p className={styles.pitem}>Choose a date and time to book your initial consultation appointment:</p>
 <br/><br/>
-
-<h3 className={styles.july}>July</h3>
-
-<ol className={styles.calendar} start="6">
-
-	<li className={styles.oitem} id={styles.lastmonth}>
-		<ol className={styles.olist} start="29">
-			<li className={styles.oitem}>29</li>
-			<li className={styles.oitem}>30</li>
-		</ol>
-	</li>
-	
-	<li className={styles.oitem} id={styles.thismonth}>
-		<ol className={styles.olist}>
-			<li className={styles.oitem}>1</li>
-			<li className={styles.oitem}>2</li>
-			<li className={styles.oitem}>3</li>
-			<li className={styles.oitem}>4</li>
-			<li className={styles.oitem}>5</li>
-			<li className={styles.oitem}>6</li>
-			<li className={styles.oitem}>7</li>
-			<li className={styles.oitem}>8</li>
-			<li className={styles.oitem}>9</li>
-			<li className={styles.oitem}>10</li>
-			<li className={styles.oitem}>11</li>
-			<li className={styles.oitem}>12</li>
-			<li className={styles.oitem}>13</li>
-			<li className={styles.oitem}>14</li>
-			<li className={styles.oitem}>15</li>
-			<li className={styles.oitem}>16</li>
-			<li className={styles.oitem}>17</li>
-			<li className={styles.oitem}>18</li>
-			<li className={styles.oitem}>19</li>
-			<li className={styles.oitem}>20</li>
-			<li className={styles.oitem}>21</li>
-			<li className={styles.oitem}>22</li>
-			<li className={styles.oitem}>23</li>
-			<li className={styles.oitem}>24</li>
-			<li className={styles.oitem}>25</li>
-			<li className={styles.oitem}>26</li>
-			<li className={styles.oitem}>27</li>
-			<li className={styles.oitem}>28</li>
-			<li className={styles.oitem}>29</li>
-			<li className={styles.oitem}>30</li>
-			<li className={styles.oitem}>31</li>
-		</ol>
-	</li>
-	
-	<li className={styles.oitem} id={styles.nextmonth}>
-		<ol>
-			<li className={styles.oitem}>1</li>
-			<li className={styles.oitem}>2</li>
-		</ol>
-	</li>
-	
-</ol>
+<input type="date" />
 <Link to="/time/">
           <button className={styles.secondarycta}>Continue to Checkout</button>
           </Link>
