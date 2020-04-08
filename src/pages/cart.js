d3daf70d14c0ae1879560aee55a702dff42b725c
nyoandnuraco <info@nyoandnuraco.com>
2020-04-07 21:17:36 -0500

    addstatetoremoveitem

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -2,9 +2,28 @@ import React from "react";
 import styles from "./cart.module.css";
 import Layout from "../components/layout";
 import logo from "../components/nyoandnuralogo.png";
-import { Link } from "gatsby"
+import { Link } from "gatsby";
 class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      removeItem: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+      e.preventDefault();
+    this.setState({
+      removeItem: true
+    });
+  }
   render() {
+    const remove = {
+      display:'none'
+    }
+    const keep ={
+      display: 'block'
+    }
     return (
       <Layout location={this.props.location}>
         <br />
@@ -22,23 +41,30 @@ class Cart extends React.Component {
             <div className={styles.progressbar}></div>
             <span className={styles.step}>4</span>
           </div>
-          <br /><br/>
+          <br />
+          <br />
           <div className={styles.sidebyside}>
-          <h2 className={styles.headercart}>Your Cart</h2>
-          <h4 className={styles.itemnumber}>1 Item</h4>
+            <h2 className={styles.headercart}>Your Cart</h2>
+            <h4 className={styles.itemnumber}>1 Item</h4>
           </div>
           <hr />
-          <h6 className={styles.cartitemdesc}>
+          <h6 style={this.state.removeItem ? remove : keep} className={styles.cartitemdesc}>
             Neldo Package: Lorem ipsum ipsum ipsum
           </h6>
-          <img alt="logo" className={styles.cartitemimg} src={logo} />
-          <br /><br/>    <p className={styles.cartitemqty}>
-            <span><strong>Quantity: </strong></span>
+          <img style={this.state.removeItem ? remove : keep} alt="logo" className={styles.cartitemimg} src={logo} />
+          <br />
+          <br />
+          <p style={this.state.removeItem ? remove : keep} className={styles.cartitemqty}>
+            <span>
+              <strong>Quantity: </strong>
+            </span>
             <select className={styles.cartitemselect}>
               <option> 1</option>
             </select>
-          </p>          <br /><br/>
-          <p className={styles.pricelabel}>
+          </p>
+          <br />
+          <br />
+          <p style={this.state.removeItem ? remove : keep} className={styles.pricelabel}>
             <span>
               <strong>Price: </strong>
             </span>
@@ -47,8 +73,10 @@ class Cart extends React.Component {
             </span>
           </p>
           <br /> <br /> <br />
-          <button className={styles.removecta}>Remove Item</button>
-          <br /> <br /> 
+          <button style={this.state.removeItem ? remove : keep} onClick={e => this.handleClick(e)} className={styles.removecta}>
+            Remove Item
+          </button>
+          <br /> <br />
           <hr />
           <p>
             Total:
@@ -56,8 +84,10 @@ class Cart extends React.Component {
               <i>$2999.00</i>
             </span>
           </p>
-<Link to="/book/">
-          <button className={styles.secondarycta}>Continue to Checkout</button>
+          <Link to="/book/">
+            <button className={styles.secondarycta}>
+              Continue to Checkout
+            </button>
           </Link>
           <br />
         </div>
