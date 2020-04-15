9625b39654da2ba0ee927eecbf9a8bd3698d4182
nyoandnuraco <info@nyoandnuraco.com>
2020-04-14 22:21:13 -0500

    add:imggallarypopup

--- src/pages/shop/neldolight.js
+++ src/pages/shop/neldolight.js
@@ -3,14 +3,17 @@ import { Link } from "gatsby";
 import Layout from "../../components/layout";
 import styles from "../shop.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import designImage from '../../pages/geometric-decoration.jpg';
 class Neldolight extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       countl: 0,
+      expandImage: false,
     };
     this.handleClick = this.handleClick.bind(this);
+    this.expandImage = this.expandImage.bind(this);
+    this.collapsePopup = this.collapsePopup.bind(this);
   }
   handleClick(e) {
     e.preventDefault();
@@ -20,6 +23,20 @@ class Neldolight extends React.Component {
     var element = document.getElementById("cartcount");
     element.classList.add("mystyle");
     }
+    expandImage(e) {
+      e.preventDefault();
+      this.setState({
+        expandImage: true,
+      });
+   
+      }
+     collapsePopup(e) {
+        e.preventDefault();
+        this.setState({
+          expandImage: false,
+        });
+     
+        }
   render() {
     const mystyle = {
       color: "white",
@@ -33,6 +50,16 @@ class Neldolight extends React.Component {
       paddingRight: "12px",
       marginTop: "-82px",
       marginLeft: "82vw",
+    };
+    const popupopen = {
+      color: "black",
+      backgroundColor: "white",
+      padding: "25px",
+      width: "90vw",
+      height: "70vh",
+      position: "absolute",
+      border: "2px solid #d8d8d8",
+      borderRadius:"5px",
     };
     const nostyle = {
       display: "none",
@@ -73,7 +100,12 @@ class Neldolight extends React.Component {
               <div className={styles.smimgbox}></div>
               <div className={styles.smimgbox}></div>
             </div>
-            <div className={styles.imgbox}></div>
+            <div onClick={(e) => this.expandImage(e)} className={styles.imgbox}></div>
+            <div style={this.state.expandImage == true ? popupopen : nostyle} id={styles.popup}>
+
+              <span onClick={(e) => this.collapsePopup(e)} className={styles.exit}>X</span>
+              <img className={styles.popupimg} src={designImage} width="100%" height="100%" />
+            </div>
             <div>
               Must have top notch maintainable custom made websites to fit your
               needs
