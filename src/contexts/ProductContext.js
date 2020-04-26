f5fac2fd57294bfe9b1ca15da957d34f0d2ff0b1
nyoandnuraco <info@nyoandnuraco.com>
2020-04-26 08:13:01 -0500

    getandsetlocalstorage

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,12 +1,17 @@
 import React, { createContext, useReducer, useEffect } from 'react';
-import uuid from 'uuid/v1';
 import { productReducer } from '../reducers/productReducer';
 
 export const ProductContext = createContext();
 
 const ProductContextProvider = (props) => {
-    const [products, dispatch] = useReducer(productReducer, []);
-
+    const [products, dispatch] = useReducer(productReducer, [], () => 
+    { 
+        const localData = localStorage.getItem('products');
+        return localData ? JSON.parse(localData) : [];
+    });
+    useEffect(() => {
+        localStorage.setItem('products', JSON.stringify(products))
+    }, [products]);
     return ( 
         <ProductContext.Provider value={{products, dispatch}}>
             { props.children }
