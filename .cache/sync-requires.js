98ab2983ee85ddf668d5392aa3cace88fb44ae82
nyoandnuraco <info@nyoandnuraco.com>
2020-05-10 09:54:27 -0500

    fix:styles

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/.cache/dev-404-page.js"))),
   "component---src-pages-addon-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/addon.js"))),
   "component---src-pages-blog-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/blog.js"))),
   "component---src-pages-book-js": hot(preferDefault(require("/Users/nyeraeldomiaty/Documents/GitHub/nn/nyoandnuraco/src/pages/book.js"))),
